ab2ea2700b2938366738baaa51aa4485005861ae
christech1117 <christech1117@gmail.com>
2017-06-09 14:25:41 +0800

    loadingbar

--- src/main.js
+++ src/main.js
@@ -3,10 +3,27 @@ import App from './App';
 import router from './router';
 import axios from 'axios';
 import ElementUI from 'element-ui';
+import VueProgressBar from 'vue-progressbar'
 // import 'element-ui/lib/theme-default/index.css';    // 默认主题
 import '../static/css/theme-green/index.css';       // 浅绿色主题
 import "babel-polyfill";
 
+
+const options = {
+  color: '#00d1b2',
+  failedColor: '#874b4b',
+  thickness: '2px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options)
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios;
 new Vue({
